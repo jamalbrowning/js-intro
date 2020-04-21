78443072e3781c7b80061b02845002f6c770fd38
Jim Browning <jamalebrowning@gmail.com>
2020-04-20 19:09:36 -0500

    challenge 1

---
+++ functions.js
@@ -0,0 +1,22 @@
+// const nuggatizer = (animal) => {
+//   return `processed ${animal}`;
+// }
+// console.log(nuggatizer('c'))
+
+// const dogBreed = (breed) => {
+//   return `My favorite dog breed is ${breed}`;
+// }
+
+// console.log(dogBreed('lab'));
+// console.log(dogBreed('poodle'));
+
+const any = (number) => {
+  return 42 - number
+}
+
+console.log(any(4));
+
+const old = (yearBorn) => {
+  return 2099 - yearBorn;
+}
+console.log(old(1990));
